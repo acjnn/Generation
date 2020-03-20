5174674f1a2b536ef165e290010f3df1ed8e2d8b
acjnn <54238806+acjnn@users.noreply.github.com>
2020-03-20 18:01:30 +0100

    Update DATABASE.java

--- UTIL/DATABASE.java
+++ UTIL/DATABASE.java
@@ -1,4 +1,4 @@
-package com.generation.base;
+package com.generation.db;
 
 import java.math.BigInteger;
 import java.sql.Connection;
@@ -13,22 +13,40 @@ import java.util.List;
 import java.util.Map;
 
 public class Database implements IDatabase
-{
-	//FACADE : SUPER INTERFACCIA CHE HA IL COMPITO DI NASCONDERE TUTTE LE CLASSI DI DIFFICILE UTILIZZO:
-	//CONNECTION, STATEMENT, RESULTSET
+{	
+	//Facade -> super interfaccia 
+	//ha il compito di nascondere tutte le classi di difficile utilizzo, Connection, Statement, ResultSet
 	private String percorso;
 	private String user;
 	private String pw;
-
-	private Connection conn;
-
-	public Database(String percorso, String user, String pw)
+	
+	private Connection conn = null;
+	
+	private static IDatabase db = null;
+	
+	public synchronized static IDatabase getInstance()
 	{
-		super();
+		if(db == null)
+			db = new Database();
+		return db;
+	}
+	
+	private Database() {}
+	
+	public void setPercorso(String percorso) {
 		this.percorso = percorso;
-		this.user = user;
-		this.pw = pw;
+	}
 
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	
+	public void apriConn()
+	{
 		try
 		{
 			Class.forName("com.mysql.jdbc.Driver");
@@ -42,62 +60,79 @@ public class Database implements IDatabase
 		{
 			System.err.println("Verifica il nome del database, hai messo quello giusto?");
 		}
-
-	} //CHIUSURA COSTRUTTORE
-
+		return ;
+	}
+	
 	public Connection getConn()
 	{
 		return conn;
 	}
+	
+	//una mappa sarà trasformata in un oggetto, quando leggiamo da db carichiamo tutte le righe delle tabelle
+	//quindi avremo bisogno di una lista di mappe, corrispondenti a tutti i nostri oggetti
 
-	//UNA MAPPA SARA' TRASFORMATA IN UN OGGETTO
-	//QUANDO LEGGIAMO DA DB CARICHIAMO TUTTE LE RIGHE DELLE TABELLE
-	//QUINDI AVREMO BISOGNO DI UNA LISTA DI MAPPE CORRISPONDENTI A TUTTI I NOSTRI OGGETTI
-
-	public List<Map<String,String>> rows(String query)  //E' UN METODO CHE PERMETTE DI TRASFORMARE TUTTE LE RIGHE DELLA TABELLA
-														//IN UNA LISTA DI MAPPE. OGNI MAPPA CORRISPONDE A UNA RIGA DELLA TABELLA
-														//CHIAVI(K) = NOME DELLA COLONNA
-														//VALORE(V) = CELLA (INCROCIO RIGA*COLONNA)
+	/**
+	 * è un metodo che permette di trasformare tutte le righe della tabella in una
+	 * lista, composta da mappe. Ogni mappa corrisponde a una riga della tabella
+	 * dove le chiavi corrispondo al nome della colonna, e il valore corrisponde
+	 * all'incrocio della riga per la colonna -> colonna id, riga 1
+	 * mappa { id=1....}
+	 */
+	public List<Map<String,String>> rows(String query)
 	{
-		List<Map<String,String>> res = new ArrayList<Map<String,String>>();  //CREIAMO LA LISTA DI APPOGGIO, DI RISPOSTA
-
-		Statement s = null; //DICHIARAZIONE DI STATEMENT, STRUMENTO CHE ESEGUE LA QUERY
+		if(conn == null)
+			apriConn();
+		//creiamo la lista di risposta
+		List<Map<String,String>> res = new ArrayList<Map<String,String>>();
+		//dichiaro s come strumento che eseguirà la query, uno Statement
+		Statement s = null;
 		try
 		{
-			s = conn.createStatement(); //CREO LO STATEMENT INRENDO LA CONN (CONNESSIONE)
-			//s ESEGUE LA QUERY CHE ARRIVA DAL PARAMETRO
-			ResultSet rs = s.executeQuery(query);  //ResultSet CI DA' LA RISPOSTA CHE CORRISPONDE ALL'INTERA TABELLA
-												   //RIGHE E METADATI COMPRESI
-
-			while(rs.next())  //CICLIAMO TUTTE LE RIGHE DELLA TABELLA. SE TROVO ANCHE SOLO UNA RIGA, PER ME QUELLA RIGA DEVE DIVENTARE UNA MAPPA.
+			//usando la connessione conn, creo lo Statement s
+			s =  conn.createStatement();
+			//s esegue la query che arriva dal parametro, otteniamo una risposta
+			//del tipo ResultSet, che corrisponde all'intera tabella
+			//righe e metadati compresi
+			ResultSet rs = s.executeQuery(query);
+			//ciclare tutte le righe della tabella
+			while(rs.next())
 			{
-				Map<String,String>mappa = new HashMap<String,String>(); //CREIAMO UNA MAPPA CHE AVRA' COME CHIAVE UNA STRING
-																		//E COME VALORE UNA STRING, PERCHE' ANCHE SE IL VALORE DI UNA COLONNA
-																		//E' DI TIPO NUMERICO, UNA STRINGA  PUO' CONTENERE ANHE ALTRO CARATTERI
-																		//DIVERSI DALLE LETTERE, QUINDI ANCHE NUMERI E CARATTERI SPECIALI.
-
-				for(int i = 1; i <= rs.getMetaData().getColumnCount(); i++)//CICLO LE COLONNE. PARTO DA 1 (PERCHE' IN SQL LA COLONNA HA COME INIDICE 1)
-																			//SCORRENDO LE COLONNE, SCORRO I METADATI, QUINDI USIAMO rs.getMetaData()
-																			//E USIAMO .getCoulumnCount() CHE CI FORNISCE IL NUMERO DELLE COLONNE
+				//se trovo anche solo una riga, per me quella riga deve diventare una mappa
+				//creiamo una mappa che avrà come chiave una String, e come valore una String
+				//perché anche se il valore di una colonna è di tipo numerico, una stringa può
+				//contenere anche altri caratteri diversi dalle lettere, quindi anche numeri e 
+				//caratteri speciali
+				Map<String,String> mappa = new HashMap<String,String>();
+				//in SQL la prima colonna ha come indice il numero 1
+				//quindi non parto a contare da 0, perché andrei prima 
+				//della prima colonna, quindi avrei un errore
+				//voglio scorrere le colonne, quindi una delle parti dei metadati
+				//quindi dobbiamo usare rs.getMetaData(), che ci fornisce il numero
+				//delle colonne con il metodo .getColumnCount()
+				//così scorriamo tutte le colonne dalla prima all'ultima compresa
+				for(int i = 1; i <= rs.getMetaData().getColumnCount(); i++)
 				{
-					mappa.put(rs.getMetaData().getColumnLabel(i), rs.getString(i));//IL NOME DELLA COLONNA LO RICAVO IN BASE AL NUMERO
-																					//DELLA COLONNA CHE STO ANALIZZANDO, i:
-																					//getColumnLabel(i) -> PRENDE IL NOME DELLA COLONNA iESIMA(E GLI EVENTUALI ALIAS)
-																					//getString(i);-> PRENDE IL VALORE DELLA RIGA ALLA COLONNA iESIMA.
-
-				}//CHIUSURA CICLO FOR
-
-			res.add(mappa); //CARICHIAMO LA MAPPA NELLA LISTA MAPPE
-
-
-			}//CHIUSURA WHILE
-		}//CHIUSURA TRY
+					/* Ogni mappa corrisponde a una riga della tabella
+					 * dove le chiavi corrispondo al nome della colonna, 
+					 * e il valore corrisponde al valore della colonna di riferimento
+					 */
+					mappa.put(	rs.getMetaData().getColumnLabel(i), //il nome della colonna lo ricavo in base al numero
+															//della colonna che sto analizzando, i:
+														//il nome della colonna lo ricavo da getColumnLabel -> prende anche gli alias
+								rs.getString(i));		//getString(i), prende il valore della riga alla colonna i-esima
+				}
+				// finito il ciclo ho riempito tutta la mappa con tutti i dati presi da una riga della tabella
+				// mettendo come chiave i nomi delle colonne e il valore il dato vero e proprio
+				// quindi carichiamo la mappa nella lista di mappe
+				res.add(mappa);
+			}
+		}
 		catch(Exception e)
 		{
 			System.err.println("Attento alla query");
 			e.printStackTrace();
 		}
-		finally
+		finally 
 		{
 			try
 			{
@@ -107,31 +142,36 @@ public class Database implements IDatabase
 			{
 				e.printStackTrace();
 			}
-		}//CHISURA FINALLY
+		}
 		return res;
-	}//CHIUSURA METODO
-
-	public BigInteger update(String sql, String...params) //params E' UN VETTORE. LA DICITURA ... SI LEGGE "VARARGS"
+	}
+	
+	public BigInteger update(String sql, String... params)
 	{
+		if(conn == null)
+			apriConn();
+		//Nonostante quei puntini params è visto come un vettore
+		//La dicitura ... si legge varargs
+		//Esempio sql : insert into artisti (nome, nazionalita) values (?,?)
+		//Esempio params:  ["Pippo","Italia"]
 		BigInteger ris = null;
 		try
 		{
 			PreparedStatement p = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-			for(int i = 0; i<params.length; i++)
-				p.setString(i+1,  params[i]);
+			for(int i=0; i<params.length; i++)
+				p.setString(i+1, params[i]);
 			p.executeUpdate();
 			ResultSet rs = p.getGeneratedKeys();
 			while(rs.next())
-			ris = BigInteger.valueOf(rs.getInt(1));
-
-		}//CHIUSURA TRY
+				ris = BigInteger.valueOf(rs.getInt(1));
+		}
 		catch(Exception e)
 		{
 			e.printStackTrace();
 		}
 		return ris;
 	}
-
+	
 	public BigInteger update(String query, Map<String,String> modifiche)
 	{
 		String sostituta = "";
@@ -139,18 +179,22 @@ public class Database implements IDatabase
 		String valori = "";
 		for(String k : modifiche.keySet())
 		{
-			sostituta += k + "=' "+ modifiche.get(k)+"',";
-			chiavi += k + ",";
-			valori += "'" + modifiche.get(k)+"',";
-		}//CHIUSURA FOR
-		sostituta = sostituta.substring(0,sostituta.length()-1);
-		chiavi = chiavi.substring(0,valori.length()-1);
-		//QUERY UPDATE TABELLA SET ? WHERE ID = NUM
+			sostituta += k + "='"+ modifiche.get(k)+"',";
+			chiavi += k+ ",";
+			valori += "'"+modifiche.get(k)+"',";
+		}
+		sostituta = sostituta.substring(0, sostituta.length()-1);
+		chiavi = chiavi.substring(0, chiavi.length()-1);
+		valori = valori.substring(0, valori.length()-1);
+		//query update tabella set ? where id = num
 		query = query.replace("chiavi", chiavi);
 		query = query.replace("valori", valori);
-		query = query.replace("?", sostituta);
-
-		//QUERY UPDATE TABELLA SET CHIAVE='VALORE',CHIAVE='VALORE' WHERE ID = NUM
-				return update(query);
+		
+		query = query.replace("?",sostituta);
+		System.out.println(query);
+		//query update tabella set chiave='valore',chiave='valore' where id = num
+		return update(query);
 	}
-}//CHIUSURA CLASS
+	
+	
+}
