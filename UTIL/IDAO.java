3f9264eb23b89093d77b81df20725e0242b6d544
acjnn <54238806+acjnn@users.noreply.github.com>
2020-03-20 18:05:08 +0100

    Update IDAO.java

--- UTIL/IDAO.java
+++ UTIL/IDAO.java
@@ -1,20 +1,20 @@
-package com.generation.base;
+package com.generation.dao;
 
 import java.math.BigInteger;
 import java.util.List;
 
-import javax.swing.text.html.parser.Entity;
+import com.generation.base.Entity;
 
-public interface IDAO
+public interface IDAO 
 {
-        static String read = "select * from tabella where id = ?;";
-        static String upadate = "update tabella set ? where id = [id];";
-        static String insert = "insert into tabella (chiavi) values (valori);";
-        static String delete = "delete from tabella where id = [id]";
-
-        public List<Entity> list();
-        public Entity load(BigInteger id);
-        public Entity load(Entity e);
-        public boolean delete(BigInteger id);
-
-}//CHIUSURA INTERFACE
+	static String read = "select * from tabella where id = ?;";
+	static String update = "update tabella set ? where id = [id];";
+	static String insert = "insert into tabella (chiavi) values (valori);";
+	static String delete = "delete from tabella where id = [id]";
+	
+	public List<Entity> list();
+	public List<Entity> list(String filtro);
+	public Entity load(BigInteger id);
+	public Entity load(Entity e);
+	public boolean delete(BigInteger id);
+}
